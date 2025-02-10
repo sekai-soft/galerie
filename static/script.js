946c9e1ce6f18f1660f4eb3ab89a73a59b022684
KTachibanaM <whj19931115@gmail.com>
2025-02-10 01:49:38 +0000

    add color to pull to refresh

--- static/script.js
+++ static/script.js
@@ -29,6 +29,57 @@ if (isIos && isStandalone) {
     onRefresh() {
       location.reload();
     },
+    getStyles() {
+      return `
+.__PREFIX__ptr {
+  box-shadow: inset 0 -3px 5px rgba(0, 0, 0, 0.12);
+  pointer-events: none;
+  font-size: 0.85em;
+  font-weight: bold;
+  top: 0;
+  height: 0;
+  transition: height 0.3s, min-height 0.3s;
+  text-align: center;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-end;
+  align-content: stretch;
+}
+
+.__PREFIX__box {
+  padding: 10px;
+  flex-basis: 100%;
+  color: #1A1A1A;
+}
+
+.__PREFIX__pull {
+  transition: none;
+}
+
+.__PREFIX__text {
+  margin-top: .33em;
+  color: #F5F5F5;
+}
+
+.__PREFIX__icon {
+  color: rgba(0, 0, 0, 0.3);
+  transition: transform .3s;
+}
+
+/*
+When at the top of the page, disable vertical overscroll so passive touch
+listeners can take over.
+*/
+.__PREFIX__top {
+  touch-action: pan-x pan-down pinch-zoom;
+}
+
+.__PREFIX__release .__PREFIX__icon {
+  transform: rotate(180deg);
+}
+`
+    }
   });
 }
 
