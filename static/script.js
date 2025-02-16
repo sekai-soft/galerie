9740f38b665ff38537ca704bbd81e3ce3a340b27
KTachibanaM <whj19931115@gmail.com>
2025-02-16 18:10:36 +0000

    replace pull to refresh with refresh button

--- static/script.js
+++ static/script.js
@@ -25,62 +25,9 @@ if (isMacSafari) {
 const isIos = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 const isStandalone = navigator.standalone || window.matchMedia("(display-mode: standalone)").matches;
 if (isIos && isStandalone) {
-  PullToRefresh.init({
-    onRefresh() {
-      location.reload();
-    },
-    getStyles() {
-      return `
-.__PREFIX__ptr {
-  box-shadow: inset 0 -3px 5px rgba(0, 0, 0, 0.12);
-  pointer-events: none;
-  font-size: 0.85em;
-  font-weight: bold;
-  top: 0;
-  height: 0;
-  transition: height 0.3s, min-height 0.3s;
-  text-align: center;
-  width: 100%;
-  overflow: hidden;
-  display: flex;
-  align-items: flex-end;
-  align-content: stretch;
-}
-
-.__PREFIX__box {
-  padding: 10px;
-  flex-basis: 100%;
-  color: #1A1A1A;
-}
-
-.__PREFIX__pull {
-  transition: none;
-}
-
-.__PREFIX__text {
-  margin-top: .33em;
-  color: #F5F5F5;
-}
-
-.__PREFIX__icon {
-  color: rgba(0, 0, 0, 0.3);
-  transition: transform .3s;
-}
-
-/*
-When at the top of the page, disable vertical overscroll so passive touch
-listeners can take over.
-*/
-.__PREFIX__top {
-  touch-action: pan-x pan-down pinch-zoom;
-}
-
-.__PREFIX__release .__PREFIX__icon {
-  transform: rotate(180deg);
-}
-`
-    }
-  });
+  for (const el of document.getElementsByClassName('refresh')) {
+    el.style.display = 'block';
+  }
 }
 
 Cookies.set('tz', Intl.DateTimeFormat().resolvedOptions().timeZone)
