09573608d8d15818b74cd3aca51157a315815fff
KTachibanaM <whj19931115@gmail.com>
2025-03-06 01:42:11 +0000

    simple carousel

---
+++ static/item.js
@@ -0,0 +1,18 @@
+let mediaIndex = 1;
+
+const nextMedia = (totalMediaCount) => {
+    const currentMedia = document.getElementById(`media-${mediaIndex}`);
+    const currentMediaOverlay = document.getElementById(`media-overlay-${mediaIndex}`);
+    currentMedia.style.display = 'none';
+    currentMediaOverlay.style.display = 'none';
+
+    mediaIndex += 1;
+    if (mediaIndex > totalMediaCount) {
+        mediaIndex = 1;
+    }
+
+    const nextMedia = document.getElementById(`media-${mediaIndex}`);
+    const nextMediaOverlay = document.getElementById(`media-overlay-${mediaIndex}`);
+    nextMedia.style.display = 'block';
+    nextMediaOverlay.style.display = 'block';
+}
