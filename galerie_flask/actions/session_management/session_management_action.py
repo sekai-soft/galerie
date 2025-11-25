ea537d8f3c45971abde206840b5ad86f38b115a1
KTachibanaM <whj19931115@gmail.com>
2025-11-24 23:29:59 -0800

    terminate all other unnamed sessions

--- galerie_flask/actions/session_management/session_management_action.py
+++ galerie_flask/actions/session_management/session_management_action.py
@@ -48,3 +48,25 @@ def terminate_all_other_sessions():
     db.session.commit()
 
     return make_toast(200, _("Terminated {} other session(s)").format(deleted_count))
+
+
+@session_management_bp.route('/terminate_all_other_unnamed_sessions', methods=['POST'])
+@requires_auth
+def terminate_all_other_unnamed_sessions():
+    session_token = request.cookies.get('session_token')
+    if not session_token:
+        return make_toast(400, _("No session token found"))
+
+    current_session = db.session.query(Session).filter_by(uuid=session_token).first()
+    if not current_session:
+        return make_toast(404, _("Current session not found"))
+
+    # Delete all unnamed sessions for this user except the current one
+    deleted_count = db.session.query(Session).filter(
+        Session.user_uuid == current_session.user_uuid,
+        Session.uuid != session_token,
+        Session.name == ""
+    ).delete()
+    db.session.commit()
+
+    return make_toast(200, _("Terminated {} unnamed session(s)").format(deleted_count))
